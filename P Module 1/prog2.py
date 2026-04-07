62e1701813e89dfa0abfd1369c9c82ac2ea26101
Hoorain-kamal <hkamal9380@gmail.com>
2026-04-07 12:40:57 +0530

    Add files via upload

---
+++ P Module 1/prog2.py
@@ -0,0 +1,8 @@
+name=input("Enter your name:")
+year=input("Enter your year of birth:")
+print(name)
+print(year)
+if year>=1966:
+    print("You are a senior citizen")
+else :
+    print("You are not a senior citizen")
